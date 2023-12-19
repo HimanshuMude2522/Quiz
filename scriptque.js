f78596b353bd7b46c497e3b6595a94a7e14404e0
Himanshu Mude <129898410+HimanshuMude2522@users.noreply.github.com>
2023-12-19 21:15:58 +0530

    Add files via upload

---
+++ scriptque.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is the primary cause of friction between two surfaces?",
+    answer: "Contact forces",
+    options: [
+      "Magnetic attraction",
+      "Contact forces",
+      "Electrostatic repulsion",
+      "Gravitational pull"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which type of friction opposes the initial motion between two surfaces?",
+    answer: "Static friction",
+    options: [
+      "Rolling friction",
+      "Static friction",
+      "Kinetic friction",
+      "Fluid friction"
+    ]
+  },
+  {
+    numb: 3,
+    question: "The force of friction depends on:",
+    answer: "The nature of the surfaces in contact",
+    options: [
+      "The surface area of the objects",
+      "The speed of the objects",
+      "The nature of the surfaces in contact",
+      "The temperature of the environment"
+    ]
+  },
+  {
+    numb: 4,
+    question: "When an object is in motion, the friction acting on it is called:",
+    answer: "Limiting friction",
+    options: [
+      "Static friction",
+      "Dynamic friction",
+      "Limiting friction",
+      "Rolling friction"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which type of friction is desirable for walking and running?",
+    answer: "Static friction",
+    options: [
+      "Static friction",
+      "Kinetic friction",
+      "Rolling friction",
+      "Fluid friction"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Lubricants are used to reduce:",
+    answer: "Kinetic friction",
+    options: [
+      "Static friction",
+      "Kinetic friction",
+      "Rolling friction",
+      "Fluid friction"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The force of friction always acts:",
+    answer: "Opposite to the direction of motion",
+    options: [
+      "In the direction of motion",
+      "Opposite to the direction of motion",
+      "Perpendicular to the direction of motion",
+      "Along the direction of motion"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which surface combination will have the highest friction?",
+    answer: "Rough wood on rough glass",
+    options: [
+      "Smooth wood on smooth glass",
+      "Rough wood on rough glass",
+      "Smooth wood on rough glass",
+      "Rough wood on smooth glass"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The coefficient of friction is a measure of:",
+    answer: "Force of friction",
+    options: [
+      "Force of friction",
+      "Mass of the object",
+      "Area of contact",
+      "Smoothness of the surfaces"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which force is responsible for the rolling friction between a wheel and the surface?",
+    answer: "Frictional force",
+    options: [
+      "Electromagnetic force",
+      "Normal force",
+      "Gravitational force",
+      "Frictional force"
+    ]
+  }
+];
